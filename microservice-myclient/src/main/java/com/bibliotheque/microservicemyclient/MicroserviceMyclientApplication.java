67c39e64a2653d397feefd57db09511b5643fa38
vinsbld <vincent.blndel@gmail.com>
2020-03-04 11:10:16 +0100

    addMclient

---
+++ microservice-myclient/src/main/java/com/bibliotheque/microservicemyclient/MicroserviceMyclientApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotheque.microservicemyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceMyclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceMyclientApplication.class, args);
+	}
+
+}
---
+++ microservice-myclient/src/test/java/com/bibliotheque/microservicemyclient/MicroserviceMyclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotheque.microservicemyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceMyclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
