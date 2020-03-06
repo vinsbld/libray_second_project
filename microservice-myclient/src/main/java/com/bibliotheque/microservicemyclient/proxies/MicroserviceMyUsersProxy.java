5a5939f99c0ca60d9cd41a24c7545ef2377976fd
vinsbld <vincent.blndel@gmail.com>
2020-03-06 23:25:13 +0100

    addRibbonToUsers

--- microservice-myclient/src/main/java/com/bibliotheque/microservicemyclient/proxies/MicroserviceMyUsersProxy.java
+++ microservice-myclient/src/main/java/com/bibliotheque/microservicemyclient/proxies/MicroserviceMyUsersProxy.java
@@ -1,11 +1,13 @@
 package com.bibliotheque.microservicemyclient.proxies;
 
 import com.bibliotheque.microservicemyclient.bean.UtilisateurBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 
-@FeignClient(name = "microservice-myusers", url = "localhost:9002")
+@FeignClient(name = "microservice-myusers")
+@RibbonClient(name = "microservice-myusers")
 public interface MicroserviceMyUsersProxy {
 
     @GetMapping("/{pseudo}/connexion")
