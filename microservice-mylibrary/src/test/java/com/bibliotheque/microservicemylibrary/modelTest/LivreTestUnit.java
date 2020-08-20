19c2a379c7f2bed466bb1a4778e592048cc64678
vinsbld <vincent.blndel@gmail.com>
2020-08-20 16:04:16 +0200

    addDependenciesPOMLibrary

---
+++ microservice-mylibrary/src/test/java/com/bibliotheque/microservicemylibrary/modelTest/LivreTestUnit.java
@@ -0,0 +1,14 @@
+package com.bibliotheque.microservicemylibrary.modelTest;
+
+import com.bibliotheque.microservicemylibrary.model.Livre;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LivreTestUnit {
+
+   private Livre livreTest = new Livre();
+
+
+   private Livre livreTest_1 = new Livre();
+
+}
